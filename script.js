b7abf43f2a97f11f04198d63bf5b258597e7c132
Briana <bwalker977@knights.ucf.edu>
2020-10-20 21:25:07 -0400

    mobile

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleClass(elem, className) {
+  if (elem.className.indexOf(className) !== -1) {
+    elem.className = elem.className.replace(className, "");
+  } else {
+    elem.className = elem.className.replace(/\s+/g, " ") + " " + className;
+  }
+  return elem;
+}
+function toggleDisplay(elem) {
+  const curDisplayStyle = elem.style.display;
+
+  if (curDisplayStyle === "none" || curDisplayStyle === "") {
+    elem.style.display = "block";
+  } else {
+    elem.style.display = "none";
+  }
+}
+
+function toggleMenuDisplay(e) {
+  const dropdown = e.currentTarget.parentNode;
+  const menu = dropdown.querySelector(".menu");
+
+  toggleClass(menu, "hide");
+}
+
+const dropdownTitle = document.querySelector(".dropdown .title");
+
+dropdownTitle.addEventListener("click", toggleMenuDisplay);
